825a05b7c80d5b5ec5da240afeaba20b39fa1e51
Sai1999ram <129557736+Sai1999ram@users.noreply.github.com>
2023-09-14 23:29:04 +0530

    Update deploy.sh
    new commit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Replace these values with your actual ECR image details
-AWS_ACCOUNT_ID="your-aws-account-id"
-AWS_REGION="your-aws-region"
-ECR_REPOSITORY="your-ecr-repository"
-DOCKER_IMAGE_TAG="your-image-tag"
+AWS_ACCOUNT_ID="650691777252"
+AWS_REGION="ap-south-1"
+ECR_REPOSITORY="codebuild"
+DOCKER_IMAGE_TAG="latest"
 
 # Log in to the ECR registry
-aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 650691777252.dkr.ecr.ap-south-1.amazonaws.com
 
 # Pull the Docker image from ECR
 docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY:$DOCKER_IMAGE_TAG
