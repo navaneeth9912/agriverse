4e219eb06002530d2f6ba0e38e3fd3227e801d3a
navaneeth <110413822+navaneeth9912@users.noreply.github.com>
2023-09-17 11:20:39 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ ECR_REPOSITORY="agresive-main"
 DOCKER_IMAGE_TAG="latest"
 
 # Log in to the ECR registry
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 650691777252.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 650691777252.dkr.ecr.us-east-1.amazonaws.com
 
 # Pull the Docker image from ECR
 docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY:$DOCKER_IMAGE_TAG
